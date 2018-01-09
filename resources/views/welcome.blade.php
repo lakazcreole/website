d71037144311f7250dd3b70b15010c64cf70a386
Strift <lau.cazanove@gmail.com>
2018-01-09 20:44:05 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+
+        <title>La Kaz Créole</title>
+    </head>
+    <body>
+        <img src="img/LaKazCréole-Logotype_Vector.svg" alt="La Kaz Créole - Logotype" id="logotype">
+        <h1>Site en construction</h1>
+        <div class="gradiant"></div>
+    </body>
+</html>
