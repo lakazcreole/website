dd5aa0223795cd62b922dc836748d82bddcc50b8
Strift <lau.cazanove@gmail.com>
2018-01-10 18:07:45 +0100

    Contact form front-end integration

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.Vue.use(require('vue-resource'))
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,7 +16,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('contact-form', require('./components/ContactForm.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,8 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
-
-        <title>La Kaz Créole</title>
-    </head>
-    <body>
-        <div class="text-center">
-            <img src="{{ asset('img/logo.svg') }}" alt="La Kaz Créole - Logotype" id="logotype">
-            <h1 class="title">Site en construction</h1>
-        </div>
-        <div class="gradiant-background"></div>
-    </body>
-</html>
+@section('content')
+    <div class="text-center">
+        <img src="{{ asset('img/logo.svg') }}" alt="La Kaz Créole - Logotype" id="logotype">
+        <h1 class="title">Site en construction</h1>
+    </div>
+@endsection
