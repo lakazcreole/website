5e2ae07b1d959e248fab27aab296596e5e141a5a
Strift <lau.cazanove@gmail.com>
2018-02-20 22:49:40 +0100

    Order accepting, declining views

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+
+        <title>
+            @section('title')
+                Dashboard - {{ config('app.name') }}
+            @show
+        </title>
+    </head>
+
+    <body>
+        <div id="app">
+            @yield('modals')
+            @yield('content')
+        </div>
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
+</html>
