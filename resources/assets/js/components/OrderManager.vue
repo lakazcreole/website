c09b8cb8f4f6362227973d8131399ee48c0cb705
Strift <lau.cazanove@gmail.com>
2018-04-19 09:23:51 +0200

    Fix cart sticky

--- resources/assets/js/components/OrderManager.vue
+++ resources/assets/js/components/OrderManager.vue
@@ -1,3 +1,83 @@
+<template>
+  <div v-if="finished" class="container">
+    <div style="height: 259px;" class="d-flex">
+      <div class="my-auto w-100">
+        <p class="my-3 text-center">Merci de votre commande ! Je reviendrai rapidement vers vous par le biais de votre adresse e-mail <strong>{{ order.customer.email }}</strong>.</p>
+      </div>
+    </div>
+  </div>
+  <div v-else>
+    <div v-if="error" class="container">
+      <p class="my-3 text-center">Le service de commande est indisponible. Veuillez réessayer plus tard.</p>
+    </div>
+    <div v-else-if="loaded" class="container">
+      <div v-if="showTimeSelector" style="height: 259px;" class="d-flex">
+        <div class="my-auto w-100">
+          <h2 class="mb-3">Livraison</h2>
+          <DeliveryTimeForm @submit="handleDeliveryTimeFormSubmit" :on-date-input="handleDateInput" :on-time-input="handleTimeInput"/>
+        </div>
+      </div>
+      <div v-else>
+        <modal v-if="showModal" @close="showModal = false">
+          <h3 slot="header">Livraison</h3>
+          <DeliveryTimeSelector
+            slot="body"
+            :default-date="order.date"
+            :default-time="order.time"
+            :on-date-input="value => { this.editedDate = value }"
+            :on-time-input="value => { this.editedTime = value }"
+          />
+          <div slot="footer" class="text-right">
+            <button type="button" class="btn btn-secondary ml-2" @click="showModal = false">Annuler</button>
+            <button type="button" class="btn btn-primary ml-2" @click="handleDeliveryTimeModalSave">Modifier</button>
+          </div>
+        </modal>
+        <div class="row">
+          <div class="col-md-8">
+            <order-menu v-if="showMenu" :products="products" :handle-add="addOrderLine"/>
+            <!-- eslint-disable vue/html-indent -->
+            <delivery-form v-if="showDeliveryForm"
+              :errors="order.errors"
+              :on-first-name-input="value => { this.order.customer.firstName = value }"
+              :on-last-name-input="value => { this.order.customer.lastName = value }"
+              :on-email-input="value => { this.order.customer.email = value }"
+              :on-phone-input="value => { this.order.customer.phone = value }"
+              :on-address-one-input="value => { this.order.address1 = value }"
+              :on-address-two-input="value => { this.order.address2 = value }"
+              :on-address-three-input="value => { this.order.address3 = value }"
+              :on-city-input="value => { this.order.city = value }"
+              :on-zip-input="value => { this.order.zip = value }"
+            />
+            <!-- eslint-enable vue/html-indent -->
+          </div>
+          <div class="col-md-4">
+            <div v-if="showBasket" v-sticky="{ zIndex: 1019, stickyTop: 115 }">
+              <cart :items="order.lines" :editable="showMenu" @validate="handleCartValidate()" @edit="handleCartEdit()" @removeItem="removeOrderLine">
+                <div slot="info" class="my-3">
+                  <div v-if="showDeliveryForm" class="form-group">
+                    <label for="inputInformation">Informations</label>
+                    <textarea @input="value => { this.order.information = value }" class="form-control" placeholder="Allergies, etc."/>
+                  </div>
+                  <p class="mb-0 text-center">
+                    Livraison le <a href="#" class="link" title="Modifier" @click.prevent="handleDeliveryTimeEdit()">{{ readableDate }} à {{ order.time }}</a>.<br>
+                    Paiement en <strong>espèces, tickets restaurant ou Lydia</strong>.
+                  </p>
+                </div>
+              </cart>
+              <div v-if="showDeliveryForm" class="mt-3">
+                <button class="btn btn-lg btn-block btn-primary" @click="handleOrder()" :disabled="!deliveryFormFilled">Commander</button>
+                <div v-if="order.serverError" class="mt-3 text-center">
+                  <p class="mb-0 text-danger">Une erreur s'est produite. Veuillez réessayer plus tard.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script>
 import axios from 'axios'
 import VueSticky from 'vue-sticky'
@@ -178,78 +258,3 @@ export default {
 }
 </script>
 
-<template>
-  <!-- eslint-disable vue/html-indent -->
-  <div v-if="finished" class="container">
-    <div style="height: 259px;" class="d-flex">
-      <div class="my-auto w-100">
-        <p class="my-3 text-center">Merci de votre commande ! Je reviendrai rapidement vers vous par le biais de votre adresse e-mail <strong>{{ order.customer.email }}</strong>.</p>
-      </div>
-    </div>
-  </div>
-  <div v-else>
-    <div v-if="error" class="container">
-      <p class="my-3 text-center">Le service de commande est indisponible. Veuillez réessayer plus tard.</p>
-    </div>
-    <div v-else-if="loaded" class="container">
-      <div v-if="showTimeSelector" style="height: 259px;" class="d-flex">
-        <div class="my-auto w-100">
-          <h2 class="mb-3">Livraison</h2>
-          <DeliveryTimeForm @submit="handleDeliveryTimeFormSubmit" :on-date-input="handleDateInput" :on-time-input="handleTimeInput"/>
-        </div>
-      </div>
-      <div v-else>
-        <modal v-if="showModal" @close="showModal = false">
-          <h3 slot="header">Livraison</h3>
-          <DeliveryTimeSelector slot="body" :default-date="order.date" :default-time="order.time"
-            :on-date-input="value => { this.editedDate = value }"
-            :on-time-input="value => { this.editedTime = value }"
-            />
-          <div slot="footer" class="text-right">
-            <button type="button" class="btn btn-secondary ml-2" @click="showModal = false">Annuler</button>
-            <button type="button" class="btn btn-primary ml-2" @click="handleDeliveryTimeModalSave">Modifier</button>
-          </div>
-        </modal>
-        <div class="row">
-          <div class="col-md-8">
-            <order-menu v-if="showMenu" :products="products" :handle-add="addOrderLine"/>
-            <delivery-form v-if="showDeliveryForm"
-              :errors="order.errors"
-              :on-first-name-input="value => { this.order.customer.firstName = value }"
-              :on-last-name-input="value => { this.order.customer.lastName = value }"
-              :on-email-input="value => { this.order.customer.email = value }"
-              :on-phone-input="value => { this.order.customer.phone = value }"
-              :on-address-one-input="value => { this.order.address1 = value }"
-              :on-address-two-input="value => { this.order.address2 = value }"
-              :on-address-three-input="value => { this.order.address3 = value }"
-              :on-city-input="value => { this.order.city = value }"
-              :on-zip-input="value => { this.order.zip = value }"
-            />
-          </div>
-          <div class="col-md-4">
-            <div v-if="showBasket" v-sticky="{ zIndex: 1020, stickyTop: 115 }">
-              <cart :items="order.lines" :editable="showMenu" @validate="handleCartValidate()" @edit="handleCartEdit()" @removeItem="removeOrderLine">
-                <div slot="info" class="my-3">
-                  <div v-if="showDeliveryForm" class="form-group">
-                    <label for="inputInformation">Informations</label>
-                    <textarea @input="value => { this.order.information = value }" class="form-control" placeholder="Allergies, etc."/>
-                  </div>
-                  <p class="mb-0 text-center">
-                    Livraison le <a href="#" class="link" title="Modifier" @click.prevent="handleDeliveryTimeEdit()">{{ readableDate }} à {{ order.time }}</a>.<br>
-                    Paiement en <strong>espèces, tickets restaurant ou Lydia</strong>.
-                  </p>
-                </div>
-              </cart>
-              <div v-if="showDeliveryForm" class="mt-3">
-                <button class="btn btn-lg btn-block btn-primary" @click="handleOrder()" :disabled="!deliveryFormFilled">Commander</button>
-                <div v-if="order.serverError" class="mt-3 text-center">
-                  <p class="mb-0 text-danger">Une erreur s'est produite. Veuillez réessayer plus tard.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
