77c1bb77d79dffd343bf5ce855f90af58affacb9
Strift <lau.cazanove@gmail.com>
2018-04-23 20:48:04 +0200

    Dashboard for orders

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -1,4 +1,40 @@
 @extends('layouts.dashboard')
 
 @section('content')
+  <div class="container">
+    @foreach($orders as $order)
+      <div class="card my-3">
+        <div class="card-header d-flex flex-row">
+          Commande #{{ $order->id }}
+          <small class="ml-auto">{{ strftime('%A %d %B %Y', strtotime($order->date)) }} at {{ $order->time }}</small>
+        </div>
+        <div class="card-body">
+            @foreach($order->lines as $line)
+              <div class="d-flex flex-row align-items-center">
+                {{ $line->quantity . ' ' }}{{ $line->product->pieces > 1 ? "portion(s) de {$line->product->pieces}" : "" }} {{ $line->product->name }}
+                <span class="ml-auto">{{ number_format($line->totalPrice, 2) }} €</span>
+              </div>
+            @endforeach
+            <div class="d-flex flex-row align-items-center">
+              <em>Livraison</em> <em class="ml-auto">{{ number_format($order->deliveryPrice, 2) }} €</em>
+            </div>
+            <div class="d-flex flex-row align-items-center">
+              <strong>Total</strong> <strong class="ml-auto">{{ number_format($order->fullPrice, 2) }} €</strong>
+            </div>
+        </div>
+        <div class="card-footer">
+          {{ $order->customer->firstName }} {{ $order->customer->lastName }} &bull; {{ $order->customer->email }}<br>
+          Tél. : {{ $order->customer->phone }}<br>
+          <em>
+            {{ $order->address1 }}<br>
+            {{ $order->address2 }}<br>
+            {{ $order->address3 }}<br>
+            {{ $order->zip }}<br>
+            {{ $order->city }}
+          </em>
+          <br>
+        </div>
+      </div>
+    @endforeach
+  </div>
 @endsection
