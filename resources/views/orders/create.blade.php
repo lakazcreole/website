c9c9fd878fa70a92d5666b68a8dbd7255a383854
Strift <lau.cazanove@gmail.com>
2018-07-18 16:58:14 +0200

    Order create view

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -6,24 +6,24 @@
 @endsection
 
 @section('content')
-    <header v-sticky="{ zIndex: 1020, stickyTop: 0 }">
-        @include('partials.navbar')
-    </header>
-    <section class="order position-relative">
-        <div class="order-header pb-4 position-relative">
-            <div class="background position-absolute w-100 h-100"></div>
-            <div class="overlay position-absolute w-100 h-100"></div>
-            <div class="container">
-                <h1 class="text-light pt-4 mb-3">Commande</h1>
-                <p class="text-light">
-                    Chaque commande est préparée par mes soins de l'achat des marchandises à la livraison chez vous. Il faut donc commander <strong>24h à l'avance</strong> pour que je puisse vous servir dans les meilleures conditions. Merci d'avance pour votre compréhension.<br>
-                    Pour toute demande particulière, <a class="link" href="#" @click.prevent="$modal.show('contact-modal')">contactez-moi directement</a> !
-                </p>
-            </div>
-        </div>
-        <div class="order-manager container-fluid my-5 px-0">
-            <order-manager></order-manager>
-        </div>
-    </section>
-    @include('partials.footer')
+  <header v-sticky="{ zIndex: 1020, stickyTop: 0 }">
+    @include('partials.navbar')
+  </header>
+  <section>
+    <div class="relative h-32">
+      <div class="absolute z-0 w-full h-full" style="background-image: url('/images/order_header.jpg'); background-size: cover; background-position: center"></div>
+      <div class="absolute z-0 bg-black opacity-25 w-full h-full"></div>
+      <div class="container mx-auto relative h-full text-grey-lightest">
+        <h1 class="">Commande</h1>
+        <p class="">
+          Chaque commande est préparée par mes soins de l'achat des marchandises à la livraison chez vous. Il faut donc commander <strong class="text-orange">24h à l'avance</strong> pour que je puisse vous servir dans les meilleures conditions. Merci d'avance pour votre compréhension.<br>
+          Pour toute demande particulière, <a class="text-grey-lightest" href="#" @click.prevent="$modal.show('contact-modal')">contactez-moi directement</a> !
+        </p>
+      </div>
+    </div>
+    <div class="order-manager container-fluid my-5 px-0">
+      <order-manager></order-manager>
+    </div>
+  </section>
+  @include('partials.footer')
 @endsection
