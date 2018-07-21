e95c711459998e36c4f12971cb5aa0810d30aac3
Strift <lau.cazanove@gmail.com>
2018-07-21 15:46:39 +0200

    Better folder structure

---
+++ resources/assets/js/components/shop/DeliveryAlert.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="border-l-4 border-green bg-green-lightest px-4 py-2 leading-normal text-green-darkest text-sm text-justify">
+    <p class="mb-1">
+      Chaque commande est préparée par mes soins de l'achat des marchandises à la livraison chez vous. C'est pour cela qu'il n'est pas <em class="italic">encore</em> possible de commander pour le jour même.
+    </p>
+    <!-- <p>Pour toute demande particulière, <button class="text-green-darkest font-bold no-underline" @click.prevent="$modal.show('contact-modal')">contactez-moi directement</button>.</p> -->
+  </div>
+</template>
---
+++ tests/Vue/shop/DeliveryInput.spec.js
@@ -0,0 +1,100 @@
+import Vuex from 'vuex'
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils'
+import expect from 'expect'
+
+import DeliveryInput from '../../../resources/assets/js/components/shop/DeliveryInput'
+import AddressInput from '../../../resources/assets/js/components/shop/AddressInput'
+import DateTimeInput from '../../../resources/assets/js/components/shop/DateTimeInput'
+import SplittableCard from '../../../resources/assets/js/components/SplittableCard'
+import store from '../../../resources/assets/js/store'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+const factory = (props = {}) => {
+  return shallowMount(DeliveryInput, {
+    mocks: {
+      $store: store
+    },
+    localVue,
+    propsData: {
+      ...props
+    }
+  })
+}
+
+describe('DeliveryInput', () => {
+
+  it('initially displays the AddressInput', () => {
+    const wrapper = factory()
+    expect(wrapper.find(AddressInput).isVisible()).toBe(true)
+  })
+
+  it('initially hides the SplittableCard', () => {
+    const wrapper = factory()
+    expect(wrapper.find(SplittableCard).isVisible()).toBe(false)
+  })
+
+  it('hides AddressInput and shows DateTimeInput when address is defined', () => {
+    const wrapper = mount(DeliveryInput, {
+      mocks: {
+        $store: store
+      },
+      localVue
+    })
+    wrapper.vm.$store.state.order.address = {
+      administrative: 'Centre-Val de Loire',
+      city: 'Orléans',
+      country: 'France',
+      name: '53 Faubourg Saint-Vincent',
+      postcode: '45000',
+      type: 'address',
+      value: '53 Faubourg Saint-Vincent, Orléans, Centre-Val de Loire, France'
+    }
+    wrapper.find(AddressInput).vm.$emit('change')
+    expect(wrapper.find(AddressInput).isVisible()).toBe(false)
+    expect(wrapper.find(DateTimeInput).isVisible()).toBe(true)
+  })
+
+  it('displays the address when AddressInput is hidden', () => {
+    const wrapper = mount(DeliveryInput, {
+      mocks: {
+        $store: store
+      },
+      localVue
+    })
+    wrapper.vm.$store.state.order.address = {
+      value: '53 Faubourg Saint-Vincent, Orléans, Centre-Val de Loire, France'
+    }
+    wrapper.find(AddressInput).vm.$emit('change')
+    expect(wrapper.find(AddressInput).isVisible()).toBe(false)
+    expect(wrapper.text()).toContain(wrapper.vm.$store.state.order.address.value)
+  })
+
+  it('displays the date and time when DateTimeInput is hidden', () => {
+    const wrapper = mount(DeliveryInput, {
+      mocks: {
+        $store: store
+      },
+      localVue
+    })
+    wrapper.vm.$store.state.order.date = new Date()
+    wrapper.vm.$store.state.order.time = '06:01'
+    wrapper.vm.$store.state.order.dateTimeFilled = true
+    expect(wrapper.find(DateTimeInput).isVisible()).toBe(false)
+    expect(wrapper.text()).toContain(wrapper.vm.$store.state.order.time)
+    expect(wrapper.text()).toContain(wrapper.vm.$store.getters['order/date'])
+  })
+
+  it('emits a filled event when DateTimeInput emits a filled event', () => {
+    const wrapper = mount(DeliveryInput, {
+      mocks: {
+        $store: store
+      },
+      localVue
+    })
+    wrapper.find(DateTimeInput).vm.$emit('filled')
+    expect(wrapper.emitted().filled).toBeTruthy()
+  })
+})
+
