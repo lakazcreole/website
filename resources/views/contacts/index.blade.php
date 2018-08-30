bbc5c5548ed96016be3f27583041462eb24e28f1
Strift <lau.cazanove@gmail.com>
2018-08-30 10:56:38 +0200

    Add date on contacts

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -6,7 +6,10 @@
   @foreach($contacts as $contact)
     <div class="card mb-3">
       <div class="card-body">
-        <h5 class="card-title">{{ $contact->subject }}</h5>
+        <h5 class="card-title d-flex">
+          {{ $contact->subject }}
+          <small class="text-muted ml-auto">{{ strftime('%A %d %B %Y', strtotime($contact->created_at)) }}</small>
+        </h5>
         <h6 class="card-subtitle mb-2 text-muted">{{ $contact->name }} &bull; {{ $contact->email }}</h6>
         <p class="card-text">{{ $contact->message }}</p>
       </div>
